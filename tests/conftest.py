ed7ba9decaacb0f7b909961420828567776d3b74
StevenLomon <steven.lennartsson@gmail.com>
2025-09-11 16:40:45 +0200

    Changed the order of lines in conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,10 @@
 import pytest, os, sys
 from fastapi.testclient import TestClient
-from main import app  # Import your FastAPI app instance
 
 # Add the project's root directory to the Python path
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from main import app  # Import your FastAPI app instance
 
 @pytest.fixture(scope="module")
 def client():
